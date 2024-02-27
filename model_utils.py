71156eaa6ef40afff65d622e68d586851028a969
GMartin-Data <gregory.martin.data@gmail.com>
2024-02-27 11:30:42 +0100

    Modify predict's output to str: 'Approved' or 'Rejected'

--- model_utils.py
+++ model_utils.py
@@ -6,6 +6,6 @@ def load_model(path: str = 'model.pkl'):
     model = joblib.load(path)
     return model
 
-def predict(model, data: pd.DataFrame) -> int:
+def predict(model, data: pd.DataFrame) -> str:
     predictions = model.predict(data)
-    return predictions[0]
+    return "Approved" if predictions[0] else "Rejected"
